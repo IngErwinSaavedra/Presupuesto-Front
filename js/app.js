00018feaefce7f95e7cbc5a01308a2e5307a1141
Jonathan Manchego Sosa <jonathan571999@gmail.com>
2022-05-27 02:28:59 -0500

    feature local storage

--- js/app.js
+++ js/app.js
@@ -4,39 +4,57 @@
 
 //Arreglo de Ingresos
 const ingresos = [
-    new Ingreso('Sueldo', 2100.00),
-    new Ingreso('Venta coche', 1500)
+  //   new Ingreso("Sueldo", 2100.0),
+  //   new Ingreso("Venta coche", 1500),
 ];
 //Arreglo de egresos
 const egresos = [
-    new Egreso('Renta departamento', 200),
-    new Egreso('Ropa', 100)
+  //   new Egreso("Renta departamento", 200),
+  //   new Egreso("Ropa", 100),
 ];
 
 //Funcion Principal que carga 3 funciones al principio de la aplicación
 let cargarApp = () => {
-    cargarCabecero();
-    cargarIngresos();
-    cargarEgresos();
-}
-
+  cargarInformacionUsuario();
+  cargarCabecero();
+  cargarIngresos();
+  cargarEgresos();
+};
+const cargarInformacionUsuario = () => {
+  const userLogged = Array.from(
+    JSON.parse(localStorage.getItem("details"))
+  ).find((user) => user.logged);
+  username.innerText = userLogged.email;
+  if (userLogged.ingresos) {
+    const nuevosIngresos = userLogged.ingresos.map(
+      (ingreso) => new Ingreso(ingreso._descripcion, ingreso._valor)
+    );
+    ingresos.push(...nuevosIngresos);
+  }
+  if (userLogged.egresos) {
+    const nuevosEgresos = userLogged.egresos.map(
+      (ingreso) => new Egreso(ingreso._descripcion, ingreso._valor)
+    );
+    egresos.push(...nuevosEgresos);
+  }
+};
 //Funcion que captura el valor de ingresos
 let totalIngresos = () => {
-    let totalIngreso = 0;
-    for (let ingreso of ingresos) {
-        totalIngreso += ingreso.valor;
-    }
-    return totalIngreso;
-}
+  let totalIngreso = 0;
+  for (let ingreso of ingresos) {
+    totalIngreso += ingreso.valor;
+  }
+  return totalIngreso;
+};
 
 //Funcion que captura el valor de egresos
 let totalEgresos = () => {
-    let totalEgreso = 0;
-    for (let egreso of egresos) {
-        totalEgreso += egreso.valor;
-    }
-    return totalEgreso;
-}
+  let totalEgreso = 0;
+  for (let egreso of egresos) {
+    totalEgreso += egreso.valor;
+  }
+  return totalEgreso;
+};
 
 /**
  * Carga mediante el dom con el getElementById 4 datos y calcula el
@@ -46,33 +64,43 @@ let totalEgresos = () => {
  */
 
 let cargarCabecero = () => {
-    let presupuesto = totalIngresos() - totalEgresos();
-    let porcentajeEgreso = totalEgresos() / totalIngresos();
-    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
-    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
-    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
-    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
-}
+  let presupuesto = totalIngresos() - totalEgresos();
+  let porcentajeEgreso = totalEgresos() / totalIngresos();
+  document.getElementById("presupuesto").innerHTML = formatoMoneda(presupuesto);
+  document.getElementById("porcentaje").innerHTML =
+    formatoPorcentaje(porcentajeEgreso);
+  document.getElementById("ingresos").innerHTML = formatoMoneda(
+    totalIngresos()
+  );
+  document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
+};
 
 const formatoMoneda = (valor) => {
-    return valor.toLocaleString('es-CL', { style: 'currency', currency: 'CLP', minimumFractionDigits: 0 });
-}
+  return valor.toLocaleString("es-CL", {
+    style: "currency",
+    currency: "CLP",
+    minimumFractionDigits: 0,
+  });
+};
 
 const formatoPorcentaje = (valor) => {
-    return valor.toLocaleString('es-CL', { style: 'percent', minimumFractionDigits: 2 });
-}
+  return valor.toLocaleString("es-CL", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  });
+};
 
 const cargarIngresos = () => {
-    let ingresosHTML = '';
-    for (let ingreso of ingresos) {
-        ingresosHTML += crearIngresoHTML(ingreso);
-    }
-    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
-}
+  let ingresosHTML = "";
+  for (let ingreso of ingresos) {
+    ingresosHTML += crearIngresoHTML(ingreso);
+  }
+  document.getElementById("lista-ingresos").innerHTML = ingresosHTML;
+};
 
 //Carga datos dinamicamente
 const crearIngresoHTML = (ingreso) => {
-    let ingresoHTML = `
+  let ingresoHTML = `
     <div class="elemento limpiarEstilos">
     <div class="elemento_descripcion">${ingreso.descripcion}</div>
     <div class="derecha limpiarEstilos">
@@ -86,77 +114,99 @@ const crearIngresoHTML = (ingreso) => {
     </div>
 </div>
     `;
-    return ingresoHTML;
-}
+  return ingresoHTML;
+};
 
 const eliminarIngreso = (id) => {
-    let indiceEliminar = ingresos.findIndex(ingreso => ingreso.id === id);
-    ingresos.splice(indiceEliminar, 1);
-    cargarCabecero();
-    cargarIngresos();
-}
+  let indiceEliminar = ingresos.findIndex((ingreso) => ingreso.id === id);
+  ingresos.splice(indiceEliminar, 1);
+  cargarCabecero();
+  cargarIngresos();
+};
 
-let pegreso = localStorage.getItem
+let pegreso = localStorage.getItem;
 
 const cargarEgresos = () => {
-    let egresosHTML = '';
-    for (let egreso of egresos) {
-        egresosHTML += crearEgresoHTML(egreso);
-    }
-    document.getElementById('lista-egresos').innerHTML = egresosHTML;
-}
+  let egresosHTML = "";
+  for (let egreso of egresos) {
+    egresosHTML += crearEgresoHTML(egreso);
+  }
+  document.getElementById("lista-egresos").innerHTML = egresosHTML;
+};
 
 const crearEgresoHTML = (egreso) => {
-    let egresoHTML = `
+  let egresoHTML = `
     <div class="elemento limpiarEstilos">
-                    <div class="elemento_descripcion">${egreso.descripcion}</div>
+                    <div class="elemento_descripcion">${
+                      egreso.descripcion
+                    }</div>
                     <div class="derecha limpiarEstilos">
-                        <div class="elemento_valor">- ${formatoMoneda(egreso.valor)}</div>
-                        <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor / totalEgresos())}</div>
+                        <div class="elemento_valor">- ${formatoMoneda(
+                          egreso.valor
+                        )}</div>
+                        <div class="elemento_porcentaje">${formatoPorcentaje(
+                          egreso.valor / totalEgresos()
+                        )}</div>
                         <div class="elemento_eliminar">
                             <button class="elemento_eliminar--btn">
                                 <ion-icon name="close-circle-outline"
-                                onClick='eliminarEgreso(${egreso.id})'></ion-icon>
+                                onClick='eliminarEgreso(${
+                                  egreso.id
+                                })'></ion-icon>
                             </button>
                         </div>
                     </div>
                 </div>
     `;
-    return egresoHTML;
-}
+  return egresoHTML;
+};
 
 let eliminarEgreso = (id) => {
-    let indiceEliminar = egresos.findIndex(egreso => egreso.id === id);
-    egresos.splice(indiceEliminar, 1);
-    cargarCabecero();
-    cargarEgresos();
-}
+  let indiceEliminar = egresos.findIndex((egreso) => egreso.id === id);
+  egresos.splice(indiceEliminar, 1);
+  cargarCabecero();
+  cargarEgresos();
+};
 
 let agregarDato = () => {
-    let forma = document.forms['forma'];
-    let tipo = forma['tipo'];
-    let descripcion = forma['descripcion'];
-    let valor = forma['valor'];
+  let forma = document.forms["forma"];
+  let tipo = forma["tipo"];
+  let descripcion = forma["descripcion"];
+  let valor = forma["valor"];
 
-    if (descripcion.value !== '' && valor.value !== '') {
-        if (valor.value > 0) {
-            if (tipo.value === 'ingreso') {
-                ingresos.push(new Ingreso(descripcion.value, +valor.value));
-                cargarCabecero();
-                cargarIngresos();
-            }
-            else if (tipo.value === 'egreso') {
-                if (valor.value > totalEgresos) {
-                    console.log("Error egreso");
-                    window.alert('Error, No se aceptan valores superiores a Total egreso');
-                } else {
-                    egresos.push(new Egreso(descripcion.value, +valor.value));
-                    cargarCabecero();
-                    cargarEgresos();
-                }
-            }
+  if (descripcion.value !== "" && valor.value !== "") {
+    if (valor.value > 0) {
+      if (tipo.value === "ingreso") {
+        ingresos.push(new Ingreso(descripcion.value, +valor.value));
+        const users = Array.from(JSON.parse(localStorage.getItem("details")));
+        users.forEach((user) =>
+          user.logged ? (user.ingresos = ingresos) : null
+        );
+        localStorage.setItem("details", JSON.stringify(users));
+        cargarCabecero();
+        cargarIngresos();
+      } else if (tipo.value === "egreso") {
+        if (valor.value > totalEgresos) {
+          console.log("Error egreso");
+          window.alert(
+            "Error, No se aceptan valores superiores a Total egreso"
+          );
         } else {
-            console.log('Error');
+          egresos.push(new Egreso(descripcion.value, +valor.value));
+          cargarCabecero();
+          cargarEgresos();
         }
+      }
+    } else {
+      console.log("Error");
     }
-}
+  }
+};
+const cerrarSesion = () => {
+  const usuariosFinalizados = Array.from(
+    JSON.parse(localStorage.getItem("details"))
+  ).map((user) => ({ ...user, logged: false }));
+
+  localStorage.setItem("details", JSON.stringify(usuariosFinalizados));
+  document.location.replace("./index.html");
+};
--- js/login.js
+++ js/login.js
@@ -1,83 +1,80 @@
 //Data storing into localstorage
 function validateForm() {
-
-    let data = localStorage.getItem('details') ? JSON.parse(localStorage.getItem('details')) : [];;
-    let formData = {
-        "name": document.getElementById("uName").value,
-        "email": document.getElementById("uEmail").value,
-        "password": document.getElementById("uPassword").value,
-        "confirmpassword": document.getElementById("confirmPassword").value
-    }
-    data.push(formData);
-    if (localStorage) {
-        localStorage.setItem("details", JSON.stringify(data));
-        console.log(localStorage);
-    }
+  let data = localStorage.getItem("details")
+    ? JSON.parse(localStorage.getItem("details"))
+    : [];
+  let formData = {
+    name: document.getElementById("uName").value,
+    email: document.getElementById("uEmail").value,
+    password: document.getElementById("uPassword").value,
+    confirmpassword: document.getElementById("confirmPassword").value,
+  };
+  data.push(formData);
+  if (localStorage) {
+    localStorage.setItem("details", JSON.stringify(data));
+    console.log(localStorage);
+  }
 }
 //Check if password is matching
 function verifyPassword(input) {
-    if (input.value != document.getElementById("uPassword").value) {
-        input.setCustomValidity("Password Must be Matching");
-    } else {
-        input.setCustomValidity("");
-    }
+  if (input.value != document.getElementById("uPassword").value) {
+    input.setCustomValidity("Password Must be Matching");
+  } else {
+    input.setCustomValidity("");
+  }
 }
 //check already registered users
 function emailExist(value) {
-    let existemail = JSON.parse(localStorage.getItem("details"));
-
-    let emailid = existemail.map((email, i, existemail) => {
-        return existemail[i].email;
-    });
-
-    let getexistemail = emailid.filter((email) => {
-        if (email == value.value) {
-            value.setCustomValidity('email exist. try something else');
-
-        } else {
-            value.setCustomValidity("");
-        }
-    });
+  let existemail = JSON.parse(localStorage.getItem("details"));
+  // [{email: 'user',...}, {email: 'user',...}]
+  let emailid = existemail.map((user) => user.email);
+  // ['user','user2']
+  let getexistemail = emailid.filter((email) => {
+    if (email == value.value) {
+      value.setCustomValidity("email exist. try something else");
+    } else {
+      value.setCustomValidity("");
+    }
+  });
 }
 //Handling bubbling
 const form = document.getElementById("registerForm");
 form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    form.reset();
-    document.getElementById("thankYou").style.display = "block";
-    form.style.display = "none";
+  e.preventDefault();
+  form.reset();
+  document.getElementById("thankYou").style.display = "block";
+  form.style.display = "none";
 });
 
 function showHide(show, hide) {
-    let showEle = document.getElementById(show);
-    let hideEle = document.getElementById(hide);
-    showEle.style.display = "block";
-    hideEle.style.display = "none";
+  let showEle = document.getElementById(show);
+  let hideEle = document.getElementById(hide);
+  showEle.style.display = "block";
+  hideEle.style.display = "none";
 }
 
 //login here
 function loginUser() {
-    let loginName = document.getElementById("uName").value;
-    let loginPass = document.getElementById("ePassword").value;
-    let matchEmail = JSON.parse(localStorage.getItem("details"));
-    let emailArray = [];
-    let passArray = [];
-    let result = matchEmail.map((email, i, matchEmail) => {
-
-        emailArray.push(matchEmail[i].email);
-        passArray.push(matchEmail[i].password);
-    });
-    // console.log(emailArray);
-    if (emailArray.indexOf(loginName) > -1 && passArray.indexOf(loginPass) > -1) {
-        console.log("You have sucsessfuly loged in");
-        document.location.replace('./presupuesto.html');
-
-    } else {
-        console.log("You have no registered with us");
+  let loginName = document.getElementById("uName").value;
+  let loginPass = document.getElementById("ePassword").value;
+  let matchEmail = Array.from(JSON.parse(localStorage.getItem("details")));
+  const userWanted = matchEmail.find(
+    (user) => user.email === loginName && user.password === loginPass
+  );
+  if (!userWanted) {
+    console.log("You have no registered with us");
+    return;
+  }
+  matchEmail.forEach((user) => {
+    if (user.email === loginName && user.password === loginPass) {
+      user.logged = true;
     }
-
+  });
+  localStorage.setItem("details", JSON.stringify(matchEmail));
+  console.log("You have successfully loged in");
+  document.location.replace("./presupuesto.html");
 }
 const loginForm = document.getElementById("logIn");
 loginForm.addEventListener("submit", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 });
